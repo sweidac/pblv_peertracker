819a3d83dfd6fd3678f5a27dc7e8cb43ccab16dc
christian <hartinge@hm.edu>
2016-03-09 21:09:09 +0100

    Komplett neue Stuktur git add -Agit add -A!

--- DeveloperTools/configureWebservers/software/install.sh
+++ DeveloperTools/configureWebservers/software/install.sh
@@ -11,10 +11,3 @@ cp /peertracker/web/uhttpd /etc/config/uhttpd
 # configure php5
 cp /peertracker/web/php.ini /etc/php.ini
 
-# create /www1 + ingredients
-mkdir /www1
-chmod 777 /www1
-cp -r /peertracker/web/serverdata/* /www1/
-
-# delete web data
-rm -rf /peertracker/web
